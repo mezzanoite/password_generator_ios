8a605057b3b08e35325cef617d43d079ceec6b7a
Usuário Convidado <Guest@PA06MICRO16.local>
2018-03-07 23:13:07 -0300

    salvando ...

--- SuperSenha/PasswordsViewController.swift
+++ SuperSenha/PasswordsViewController.swift
@@ -10,17 +10,36 @@ import UIKit
 
 class PasswordsViewController: UIViewController {
     
+    @IBOutlet weak var tvPasswords: UITextView!
+    
     var numberOfCharacters: Int = 10
     var numberOfPasswords: Int = 1
     var useLetters: Bool = true
     var useNumbers: Bool = true
     var useCapitalLetters: Bool = true
     var useSpacialCharacters: Bool = true
+    
+    var passwordGenerator: PasswordGenerator?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        passwordGenerator = PasswordGenerator(numberOfCharacters: numberOfCharacters, numberOfPasswords: numberOfPasswords, useLetters: useLetters, useNumbers: useNumbers, useCapitalLetters: useCapitalLetters, useSpacialCharacters: useSpacialCharacters)
+        
+        generatePassowrds()
     }
-
     
-
+    func generatePassowrds() {
+        tvPasswords.text = ""
+        guard let passwords = passwordGenerator?.generate() else {return}
+        for password in passwords {
+            tvPasswords.text.append(contentsOf: "\(password)\n\n")
+        }
+    }
+    
+    @IBAction func refresh(_ sender: Any) {
+        generatePassowrds()
+    }
+    
+    
+    
 }
---
+++ SuperSenha/TipsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TipsViewController.swift
+//  SuperSenha
+//
+//  Created by Usuário Convidado on 07/03/18.
+//  Copyright © 2018 FIAP. All rights reserved.
+//
+
+import UIKit
+
+class TipsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
